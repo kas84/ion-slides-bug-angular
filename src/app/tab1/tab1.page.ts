c8fa8d5e913208b131946f1fb8613b9b70bfbb27
Pablo Caselas <pablo.caselas@solvver.com>
2020-11-01 14:17:32 +0100

    init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
-
+  slideOpts = {
+    loop:true,
+    initialSlide: 1,
+    speed: 400,
+  }
+  activate() {
+    alert('clicked')
+  }
 }
